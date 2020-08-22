92372727e867f299059622ed08527c78d70f9262
Shubham Kumar <shubham.snu@gmail.com>
2020-08-22 16:57:07 +0530

    Added Repository intefaces for models

--- suit-up-backend/src/main/java/com/recruitathon/suitup/model/Certificate.java
+++ suit-up-backend/src/main/java/com/recruitathon/suitup/model/Certificate.java
@@ -35,4 +35,100 @@ public class Certificate {
 	
 	@Column(name="expiry")
 	private Date expiryDate;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Candidate getCandidate() {
+		return candidate;
+	}
+
+	public void setCandidate(Candidate candidate) {
+		this.candidate = candidate;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getIssuedBy() {
+		return issuedBy;
+	}
+
+	public void setIssuedBy(String issuedBy) {
+		this.issuedBy = issuedBy;
+	}
+
+	public Date getExpiryDate() {
+		return expiryDate;
+	}
+
+	public void setExpiryDate(Date expiryDate) {
+		this.expiryDate = expiryDate;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((candidate == null) ? 0 : candidate.hashCode());
+		result = prime * result + ((expiryDate == null) ? 0 : expiryDate.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((issuedBy == null) ? 0 : issuedBy.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Certificate other = (Certificate) obj;
+		if (candidate == null) {
+			if (other.candidate != null)
+				return false;
+		} else if (!candidate.equals(other.candidate))
+			return false;
+		if (expiryDate == null) {
+			if (other.expiryDate != null)
+				return false;
+		} else if (!expiryDate.equals(other.expiryDate))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (issuedBy == null) {
+			if (other.issuedBy != null)
+				return false;
+		} else if (!issuedBy.equals(other.issuedBy))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Certificate [id=" + id + ", candidate=" + candidate + ", name=" + name + ", issuedBy=" + issuedBy
+				+ ", expiryDate=" + expiryDate + "]";
+	}
+	
 }
---
+++ suit-up-backend/src/main/java/com/recruitathon/suitup/repository/CandidateRepository.java
@@ -0,0 +1,7 @@
+package com.recruitathon.suitup.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CandidateRepository extends JpaRepository<CandidateRepository, Integer>{
+
+}
