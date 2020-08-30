c88716c54289c3066d9b6afc8a7ce02c305d5450
Shubham Kumar <shubham.snu@gmail.com>
2020-08-30 16:20:12 +0530

    Added Global Exception Handlers

---
+++ suit-up-backend/src/main/java/com/recruitathon/suitup/exception/GlobalExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.recruitathon.suitup.exception;
+
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+	@ExceptionHandler(value = UserAlreadyExistsException.class)
+	public ResponseEntity<ErrorResponse> handleGenericNotFoundException(UserAlreadyExistsException e) {
+		ErrorResponse error = new ErrorResponse("USER_ALREADY_EXISTS_ERROR", e.getMessage());
+		error.setTimestamp(LocalDateTime.now());
+		error.setStatus((HttpStatus.INTERNAL_SERVER_ERROR.value()));
+		return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+
+	}
+	
+	@ExceptionHandler(value = InvalidCredentialsException.class)
+	public ResponseEntity<ErrorResponse> handleGenericNotFoundException(InvalidCredentialsException e) {
+		ErrorResponse error = new ErrorResponse("UNAUTHORIZED_ERROR", e.getMessage());
+		error.setTimestamp(LocalDateTime.now());
+		error.setStatus((HttpStatus.UNAUTHORIZED.value()));
+		return new ResponseEntity<>(error, HttpStatus.UNAUTHORIZED);
+
+	}
+
+}
