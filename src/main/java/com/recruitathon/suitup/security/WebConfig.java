729bf2e6edd47b2079f09c34c1aed8f73be57572
Shubham Kumar <shubham.snu@gmail.com>
2020-09-12 17:59:44 +0530

    Added CORS policy settings

--- src/main/java/com/recruitathon/suitup/security/WebConfig.java
+++ src/main/java/com/recruitathon/suitup/security/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**").allowedMethods("*").allowedOrigins("http://localhost:4200");
+		registry.addMapping("/**").allowedMethods("*").allowedOrigins("https://suitup-backend.herokuapp.com/");
 	}
 
 }
